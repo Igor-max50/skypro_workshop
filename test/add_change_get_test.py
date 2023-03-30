3ed3574f7f8163a3dd406d1c9e5632cc24e2f444
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-03-30 23:09:37 +0500

    test add change get

--- test/add_change_get_test.py
+++ test/add_change_get_test.py
@@ -4,22 +4,13 @@ def test_add_change_get():
     body = {"title": "My task","completed": False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json = body)
     id = response.json()["id"]
-    
-    print("получили id")
-    print(id)
 
     body = {"completed": True}
     path = "https://todo-app-sky.herokuapp.com/{id}".format(id = id, json = body)
 
-    print("путь")
-    print(path)
+    response = requests.patch(path, json = body)
 
-    response = requests.patch("https://todo-app-sky.herokuapp.com/{id}".format(id = id, json = body))
-
-    response = requests.get("https://todo-app-sky.herokuapp.com/{id}".format(id = id))
+    response = requests.get(path.format(id = id))
     completed = response.json()["completed"]
 
-    print("получили ответ")
-    print(completed)
-
-    assert response['completed'] == True
+    assert response.json()['completed'] == True
